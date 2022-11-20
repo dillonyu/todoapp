c61a9377fa21061ce0cc37b4795189dd69a11615
dillonyu <86173315+dillonyu@users.noreply.github.com>
2022-11-20 13:36:00 -0500

    Add task headings

--- src/Header.js
+++ src/Header.js
@@ -7,26 +7,46 @@ import MenuIcon from '@mui/icons-material/Menu';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 export default function Header() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar sx={{ justifyContent: 'space-between' }}>
-          <div></div>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <MenuIcon sx={{ textAlign: 'center' }} />
-            <Typography>FRAMEWORKS</Typography>
-          </div>
-          <Button
-            variant="contained"
-            color="primary"
-            startIcon={<AddCircleIcon />}
-          >
-            ADD
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <div className="header">
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar sx={{ justifyContent: 'space-between' }}>
+            <div></div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <MenuIcon sx={{ textAlign: 'center' }} />
+              <Typography>FRAMEWORKS</Typography>
+            </div>
+            <Button
+              variant="contained"
+              color="primary"
+              startIcon={<AddCircleIcon />}
+            >
+              ADD
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Card>
+        <CardContent
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-around',
+          }}
+        >
+          <Typography color="text.secondary">Title</Typography>
+          <Typography color="text.secondary">Description</Typography>
+          <Typography color="text.secondary">Deadline</Typography>
+          <Typography color="text.secondary">Priority</Typography>
+          <Typography color="text.secondary">Is Complete</Typography>
+          <Typography color="text.secondary">Action</Typography>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
